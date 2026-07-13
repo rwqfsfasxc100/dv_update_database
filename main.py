bb1cd1575837be0e5989fcb68a88f6f6f2aef7b7
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-07-13 12:57:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,42 @@ import requests
 import os
 
 try:
-  access_token = os.environ["TOKEN_SECRET"]
+    access_token = os.environ["TOKEN_SECRET"]
 except KeyError:
-  access_token = "Token not available!"
+    access_token = "Token not available!"
 
 
 def fetch_api(url,token):
-  headers = {
-    "Authorization": f"token {token}",
-  }
-  
-  response = requests.fetch(url,headers)
-  if response.status_code == 200:
-    updated_repo_data = response.json()
-    return updated_repo_data
-else:
-    return None
+    headers = {
+        "Authorization": f"token {token}",
+    }
+    response = requests.get(url,headers)
+    if response.status_code == 200:
+        updated_repo_data = response.json()
+        return updated_repo_data
+    else:
+        return None
 
 def fetch_topic_page(page_no,token):
-  headers = {
-    "Authorization": f"token {token}",
-  }
-  
-  response = requests.fetch('https://api.github.com/search/repositories?q=topic:delta-v-rings-of-saturn&per_page=100&page=%s' % page_no,headers)
-  if response.status_code == 200:
-    updated_repo_data = response.json()
-    return updated_repo_data
+    headers = {
+        "Authorization": f"token {token}",
+    }
+    url = 'https://api.github.com/search/repositories?q=topic:delta-v-rings-of-saturn&per_page=100&page=%s' % page_no
+    print(url)
+    response = requests.get(url,headers)
+    if response.status_code == 200:
+        updated_repo_data = response.json()
+        print("GOT_DATA")
+        return updated_repo_data
+    else:
+        print("FAILED_FETCH")
+        return None
+
+current_page = 1
+
+data = fetch_topic_page(current_page,"")
+
+if data:
+    print(data)
 else:
-    return None
+    print("FAIL")
